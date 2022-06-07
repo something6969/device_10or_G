a2485f4e34cbb9bcf9ac8c2193e9735fae433445
noobyysauraj <gitsauraj@gmail.com>
2022-06-07 10:45:55 +0000

    G-user 8.1.0 OPM1.171019.019 10or_G_V1_0_82 release-keys
    Signed-off-by: noobyysauraj <gitsauraj@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,9 @@
+DEVICE_PATH := device/10or/G
+BOARD_VENDOR := 10or
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2022-05-05
+
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
+
+-include vendor/10or/G/BoardConfigVendor.mk
---
+++ device.mk
@@ -0,0 +1,19 @@
+# Low power Whitelist
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+
+# QTI
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml \
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    ueventd.qcom.rc \
+
+# Inherit vendor
+$(call inherit-product, vendor/10or/G/G-vendor.mk)
